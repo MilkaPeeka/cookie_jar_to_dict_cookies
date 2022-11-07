a414267203a3abb2f3e4bce3845a785793cd49f6
MilkaPeeka <75909725+MilkaPeeka@users.noreply.github.com>
2022-11-07 16:39:33 +0200

    Create cookie_jar_to_dict_cookies.py

---
+++ cookie_jar_to_dict_cookies.py
@@ -0,0 +1,14 @@
+def cookie_jar_to_dict_cookies(filename):
+    cookies = MozillaCookieJar(filename)
+    cookies.load(ignore_discard=True, ignore_expires=True)
+
+    cookies_list = []
+    for cookie in iter(cookies):
+        cookies_list.append({
+            "name": cookie.name,
+            "value": cookie.value,
+            "domain": cookie.domain,
+            "path": cookie.path
+        })
+
+    return cookies_list
